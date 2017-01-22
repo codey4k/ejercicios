af22240b9d48da6e4743f7b7c873c4c7aa37152e
codey4k <codey4k@gmail.com>
2017-01-22 00:01:38 +0000

    practica 21/01/2017

---
+++ devJAVA/reverse_app.java
@@ -0,0 +1,16 @@
+class reverse {
+
+    public static void main(String args[]){
+        int arreglo[] = new int[4];
+
+
+        for(int n = 0; n < 4; n++){
+            arreglo[n] = n;
+        }
+
+        for(int i = 0; i < 4; i++){
+            System.out.println(arreglo[i]);
+        }
+    }
+
+}
