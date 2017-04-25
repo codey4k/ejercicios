e25dac643e8b5192ea55d4cdc6e26cde9c18385b
codey4k <codey4k@gmail.com>
2017-04-25 22:03:11 +0000

    25/04/2017

---
+++ devJAVA/vectors.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+class ejercicio1 {
+    
+    public static void llenarVector(){
+        Scanner dato = new Scanner (System.in);
+        
+        
+        int n, k = 0;
+        System.out.print("Ingrese el tamaño del vector: ");
+        n = dato.nextInt();
+        
+        int vector[] = new int[n];
+        
+        while(k < n) {
+            System.out.print("Ingrese un numero entero: ");
+            vector[k] = dato.nextInt();
+            k++;
+            
+        }
+        procesar(vector,n);
+            
+    }
+    
+    public static void procesar(int vector[], int t) {
+        int i = 1, suma = 0;
+        
+        while(i < t) {
+            suma = vector[i] + suma;
+            i += 2;
+        }
+        
+        System.out.println("La suma de los datos es: " + suma);
+        
+    }
+    
+    public static void main(String []args){
+       llenarVector();
+    }
+}
