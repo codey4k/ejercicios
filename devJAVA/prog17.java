fb5f80c2fb3e540d3bf2e5eab41a2778751504e4
codey4k <codey4k@gmail.com>
2017-08-01 23:43:12 +0000

    01/07/2017

---
+++ devJAVA/prog17.java
@@ -0,0 +1,31 @@
+//sobrecarga de constructores
+import java.util.Scanner;
+
+class Empleado {
+
+    //private final int N = 4;
+
+    //primer constructor
+    Empleado(String nom, double s, String c ){
+        nombre = nom;
+        sueldo = s;
+        carg = c;
+    }
+    
+    //segundo constructor
+    Empleado(String nom){
+        //aca se asignan datos por defecto
+        this(nom, 100.000, simple);
+    }
+    
+    public static void main(String[] args){
+        
+    }
+}
+
+
+
+
+
+
+
---
+++ devJAVA/prog18.java
@@ -0,0 +1,10 @@
+//tipo de dato enum
+class EjemploEnum {
+    enum Numeros {
+        private
+    }
+
+    public static void main(String[] args){
+        
+    }
+}
