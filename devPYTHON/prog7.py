3d34bd5c8b4287e6f3a53bef1f91cee2faf8f79f
codey4k <codey4k@gmail.com>
2017-08-06 23:09:09 +0000

    06/08/2017

--- devPYTHON/prog7.py
+++ devPYTHON/prog7.py
@@ -1,4 +1,7 @@
-nombre = "Yakiel"
+from __future__ import print_function
 
-for i in nombre:
-    print(i)
+for i in "CodeY4k":
+    print(i, end=" ")
+
+for n in range(1, 10):
+    print (f"valor {n}")
