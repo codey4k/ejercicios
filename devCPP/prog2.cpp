1302d2ea0c6785e2e63841e388e88df73b7ee03a
codey4k <codey4k@gmail.com>
2018-01-26 20:57:31 +0000

    26/01/2018

--- devCPP/prog2.cpp
+++ devCPP/prog2.cpp
@@ -2,6 +2,22 @@
 
 using namespace std;
 
-int main(){
-    
+int recursive(int n);
+
+int main(void){
+    int num;
+    cout << "Ingresa valor: ";
+    cin >> num;
+    cout << "Tu valor aumento  a: "<< recursive(num) << endl;
+
+    return 0;
+}
+
+int recursive(int n) {
+    if(n < 10) {
+        return recursive(n+1);
+    }   
+    else {
+        return n;
+    }
 }
