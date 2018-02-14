a4207f4ac972c0f4ac99101da4bf85d1b7131ab4
codey4k <codey4k@gmail.com>
2018-02-14 21:06:38 +0000

    14/02/2018

---
+++ devJAVA/prog19.java
@@ -0,0 +1,34 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+class Excepciones {
+    public static void programa() {
+        Scanner dato =  new Scanner(System.in);
+        
+        int n;
+
+        while(true) {
+            try{
+                System.out.print("Ingresa valor: ");
+                n = dato.nextInt();
+                // System.out.println("Tu valor es Valido");
+                if(n == 0){
+                    System.out.println("Adios...");
+                    break;
+                }
+                else{
+                    System.out.println("Tu valor es valido");
+                }
+            }
+            catch(InputMismatchException ex){
+                dato.nextLine();
+                System.out.print("Valor invalido");
+            }
+        }
+    }
+    
+    public static void main(String[] args) {
+        Excepciones e = new Excepciones();
+        e.programa();
+    }
+}
