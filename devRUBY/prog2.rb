d1189d5c94dcea808805be6532939d9e5fb81b93
codey4k <codey4k@gmail.com>
2018-03-19 23:11:29 +0000

    19/03/2018

--- devRUBY/prog2.rb
+++ devRUBY/prog2.rb
@@ -1,1 +1,22 @@
-puts "Hola mundo"
+class Persona
+    
+    def initialize(nombre, edad)
+        @nombre = nombre
+        @edad = edad
+    end
+    
+    def saludo()
+        puts "Hola #{@nombre}, tu edad es: #{@edad}"
+    end
+    
+end
+
+print "Ingresa nombre: "
+nombre = gets.chomp
+
+print "ingresa tu edad: "
+edad = gets.chomp
+
+persona = Persona.new(nombre, edad)
+
+persona.saludo()
