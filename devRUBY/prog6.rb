b5b9c84b3564c8a47b8286d84f2983203919903c
codey4k <codey4k@gmail.com>
2018-04-20 21:09:20 +0000

    20/04/2018

---
+++ devRUBY/prog6.rb
@@ -0,0 +1,17 @@
+# Herencia
+
+class Humano
+    def inicialize(nombre)
+        @nombre = nombre
+    end
+end
+
+class Hombre < Humano
+    def
+    end
+end
+
+class Mujer < Humano
+    def
+    end
+end
