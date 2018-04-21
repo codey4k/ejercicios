115e7468ee8d22787292e44c51047cea6f8b2a66
codey4k <codey4k@gmail.com>
2018-04-21 23:03:09 +0000

    21/04/2018

--- devRUBY/prog6.rb
+++ devRUBY/prog6.rb
@@ -1,17 +1,33 @@
 # Herencia
 
 class Humano
-    def inicialize(nombre)
+    # attr_accessor :genero, :nombre
+    
+    def initialize(nombre)
         @nombre = nombre
+    end
+    
+    def saludo
+        puts "Hola #{@nombre}, eres #{@genero}"
     end
 end
 
 class Hombre < Humano
-    def
+    def initialize(nombre)
+        @genero = "chico"
+        super(nombre)
     end
 end
 
 class Mujer < Humano
-    def
+    def initialize(nombre)
+        @genero  = "chica"
+        super(nombre)
     end
 end
+
+yakiel = Hombre.new("Yakiel")
+yakiel.saludo
+    
+emii = Mujer.new("Emilly")
+emii.saludo
