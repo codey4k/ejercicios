22feda86172b0f358f676feee47845c050c3d409
codey4k <codey4k@gmail.com>
2018-06-06 22:17:46 +0000

    06/06/2018

--- devJAVA/prog20.java
+++ devJAVA/prog20.java
@@ -1,10 +1,20 @@
 class Single {
     
-    Single() {}
+    public void hilo() {
+        try() {
+            System.out.println("cargando...")
+            
+        }
+        catch(InterruptedException e){
+            System.out.prinln("error");
+        }
+    }
     
     
     
     public static void main(String[] args) {
+        Single e = new Single();
+        e.hilo();
     }
 
 }
