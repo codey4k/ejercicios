d68e969deef1de291b5854d1bfbf5a00c66b6176
codey4k <codey4k@gmail.com>
2019-04-28 23:27:50 +0000

    28/04/2019

--- devASSEMBLY/ASM/prog8.asm
+++ devASSEMBLY/ASM/prog8.asm
@@ -1,12 +1,29 @@
+%macro imprime 3
+    mov eax, %1
+    mov ebx, 2
+    mov ecx, %2
+    mov edx, %3
+    int 0x80
+%endmacro
+
+SYS_CALL_WRITE equ 4
+
 section .text
 global _start
     _start:
+        jmp inicio
+
         inicio:
-            ;code
+            imprime SYS_CALL_WRITE, saludo, len_saludo   
+            ;1jmp inicio
+            
         finalizar:
             mov eax, 1
             mov ebx, 0
             int 0x80
 
 section .data
-section .bss
+    saludo db "Hello world", 0x0A
+    len_saludo equ $-saludo
+    
+;section .bss
--- devHASKELL/prog7.hs
+++ devHASKELL/prog7.hs
@@ -4,4 +4,4 @@ main = do
     putStrLn "Dame un numero entero "
     n <- getLine
     let numero = (read n :: Int)
-    print n
+    print numero
