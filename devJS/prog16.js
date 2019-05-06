ee8f198531c2bdf36a3e3b5ae909a41be4b21174
codey4k <codey4k@gmail.com>
2019-05-06 23:12:37 +0000

    06/05/2019

--- devJS/prog16.js
+++ devJS/prog16.js
@@ -1,4 +1,18 @@
-let arr = [1,2],
-    lista = [...arr, 3, 4]
+// let arr = [1,2],
+//     lista = [...arr, 3, 4]
 
-console.log(lista[0])
+// console.log(lista[0])
+
+//Solucion Codewars
+
+function calculate(...args){
+	let ages = args
+		.slice()
+		.map(n => n * n)
+		.reduce((a, b) => a + b, 0)
+	
+	let total = Math.sqrt(ages) / 2
+	console.log(Math.trunc(total) )
+}
+
+calculate(65,60,75,55,60,63,64,45)
--- devJS/prog9.js
+++ devJS/prog9.js
@@ -1,5 +1,22 @@
-'use-strict'
+/*'use-strict'
 
 let app = x => x * 3
 
 console.log(app(5))
+*/
+
+//SOlucion Codewars
+
+const cadena = 'Yakiel Forta'
+
+function abbrevName(name) {
+	let str = name.split(' '),
+		 inicial = ''
+	for(let i = 0; i < str.length; i++) {
+		inicial += str[i].substr(0, 1)
+	}
+
+	return inicial.split('').join('.')
+}
+
+console.log(abbrevName(cadena))
