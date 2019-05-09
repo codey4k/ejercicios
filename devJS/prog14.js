b844be74e0390a80ce31940c4b2135b73e062ab1
codey4k <codey4k@gmail.com>
2019-05-09 23:12:32 +0000

    09/05/2019

--- devJS/prog14.js
+++ devJS/prog14.js
@@ -1,6 +1,22 @@
 //contatenar elementos de una lista sin usar concat ni join
+/*
 const lista = ['L', 'u', 'i', 's']
 
 let reductor = lista.reduce((p,c)=>p +c, '')
 
 console.log(reductor)
+*/
+
+// Codewars
+function perro(...args){
+	let calculo = args
+		.slice()
+		.map(n => n * n)
+		.reduce((a, b) => a + b, 0)
+	
+    let total = Math.sqrt(calculo) / 2
+
+
+console.log(Math.trunc(total) )
+
+}
--- devJS/prog8.js
+++ devJS/prog8.js
@@ -1,7 +1,22 @@
+/*
 'use-strict'
 
 var iterable = 'codey4k'
 
 for(var i of iterable){
     console.log(i)
+}
+*/
+
+// Codewars
+
+function abbrevName(name) {
+	let cosa = name.split(' '),
+		 inicial = ''
+	for(let i = 0; i < cosa.length; i++) {
+		inicial += cosa[i].substr(0, 1)
+	}
+	
+	//return inicial.split('').join('.').toUpperCase()
+	return inicial.split('').join('.').toUpperCase()
 }
